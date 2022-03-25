9690a38f21b949819e89580cc31ce283308d5437
Cristian Osorio <Crisdevenginer@gmail.com>
2022-03-25 14:25:39 -0500

    Implementacion de router y navbar

--- angular-white-list/src/app/app.module.ts
+++ angular-white-list/src/app/app.module.ts
@@ -4,16 +4,26 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component'
+
+const routes : Routes = [
+  {path : ' ', redirectTo: 'home', pathMatch: 'full'},
+  {path : 'home', component: ListaDestinosComponent},
+  {path : 'destino', component: DestinoDetalleComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinoViajeComponent,
-    ListaDestinosComponent
+    ListaDestinosComponent,
+    DestinoDetalleComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
